716282647ab4e2746752293c4c9f11afb072fa2e
00kamil00 <kamilkalemba6@gmail.com>
2026-09-16 00:24:17 +0200

    Implement recursive fibonacci solution

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ function fibs(n) {
 }
 
 console.log(fibs(8))
+
+
+
+
+function fibsRec(n) {
+    if (n <= 0) return []
+    if (n === 1) return [0]
+    if (n === 2) return [0, 1]
+
+    const prev = fibsRec(n - 1)
+    prev.push(prev[prev.length - 1] + prev[prev.length - 2])
+    return prev
+}
+
+console.log(fibsRec(8))
