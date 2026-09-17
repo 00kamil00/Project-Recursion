ed657a1b1e1e9e2e9d6fe62506972f7ed5a050e3
00kamil00 <kamilkalemba6@gmail.com>
2026-09-17 19:13:46 +0200

    Implement helper merge function for merge sort

--- script.js
+++ script.js
@@ -31,3 +31,26 @@ function fibsRec(n) {
 }
 
 console.log(fibsRec(8))
+
+
+
+
+
+
+function merge(left, right) {
+    let result = []
+    let i = 0
+    let j = 0
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            result.push(left[i])
+            i++
+        } else {
+            result.push(right[j])
+            j++    
+        }
+    }
+    return result.concat(left.slice(i)).concat(right.slice(j))
+}
+
+console.log(merge([1, 4, 7], [2, 3, 8]))
